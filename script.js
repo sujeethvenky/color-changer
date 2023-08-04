b038923a6760542d7558b08eb094a03083f51e4a
y v sujeeth <venkatsujeeth80@gmail.com>
2023-08-04 20:29:17 +0530

    stage 2 final output

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("button");
+const randomcolor = () =>{
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+    for(let i=0;i<6;i++)
+    {
+        cons = cons + val[Math.floor(Math.random()*16)] ;
+        
+    }
+    return cons;
+};
+
+console.log(randomcolor());
+
+function changecolor(){
+    document.body.style.backgroundColor = randomcolor();
+}
+btn.addEventListener("click",changecolor);
